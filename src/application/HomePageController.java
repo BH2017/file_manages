0500c13d36c8ed4b7589be940504ea692ad4f39e
breaverheart1314 <breaverheart1314@outlook.com>
2017-06-24 10:25:16 +0800

    -----add----javaFX-----

---
+++ src/application/HomePageController.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package application;
+
+import javafx.event.ActionEvent;
+
+/**
+ * @author zzf
+ *@date 2017年6月24日 上午10:19:11
+ */
+public class HomePageController {
+	public void doSearch(ActionEvent actionEvent){
+		
+	}
+}
---
+++ src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+//import javafx.scene.layout.FlowPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			SplitPane root = (SplitPane)FXMLLoader.load(getClass().getResource("homePage.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("本地文件管理");
+		
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
